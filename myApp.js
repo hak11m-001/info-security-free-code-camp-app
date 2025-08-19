ff54070716c7372526977d649d1a8988615d5992
Hakiim <anonitsmeh@gmail.com>
2025-08-19 14:00:57 +0800

    Use helmet noSniff to Avoid Inferring the Response MIME Type

--- myApp.js
+++ myApp.js
@@ -5,7 +5,8 @@ const app = express();
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter())
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
 
 //test input name
 function displayName() {
