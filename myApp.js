e9d0bd9b53f4937f10341d31e4e877a0159c6c1d
Hakiim <anonitsmeh@gmail.com>
2025-08-22 12:28:03 +0800

    Add contentSecurityPolicy to protect  app from XSS vulnerabilities, undesired tracking, malicious frames, and much more.

--- myApp.js
+++ myApp.js
@@ -12,6 +12,12 @@ const timeInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com'],
+  },
+}));
 
 //test input name
 function displayName() {
